ef6135b3322587f9d8e9636dc7a8a530c432c19a
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2022-03-30 00:46:48 +0530

    done editor and cursor components

--- client/components/cursor.tsx
+++ client/components/cursor.tsx
@@ -6,8 +6,43 @@ type CursorProps = {
   children: string;
 }
 
+const getDefinedStyle = (classType: string) => {
+  if (classType === 'active') {
+    return {
+      color: 'white',
+      backgroundColor: 'green'
+    }
+  }
+  else if (classType === 'done') {
+    return {}
+  } 
+  else if (classType === 'incorrect') {
+    return {
+      color: 'white',
+      backgroundColor: 'red'
+    }
+  }
+  else if (classType === 'active-arrow') {
+    return {color: 'red'}
+  }
+  else if (classType === 'return') {
+    return {
+      backgroundColor: '#fc5203',
+      paddingLeft: '7px',
+      paddingRight: '3px',
+      marginLeft: '3px',
+      color: 'white',
+      fontSize: '14px'
+    }
+  }
+}
+
 function Cursor(props: CursorProps): JSX.Element {
-  return <span className={props.class} id={String(props.activeKey)}> 
+  if (props.class === undefined)
+  return <span className={props.class} id={String(props.activeKey)}>
+    {props.children}
+  </span>
+  return <span className={props.class} id={String(props.activeKey)} style={getDefinedStyle(props.class)}> 
     {props.children}
   </span>
 }
